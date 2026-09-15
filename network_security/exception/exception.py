cf4f5158c3d746b382f134d1f8d6a2ed0b759337
lezzycan <waheedolalekan23@gmail.com>
2026-09-15 18:26:30 +0100

    Refactor exception handling in NetworkSecurityException and add data extraction and MongoDB insertion functionality in push_data.py

--- network_security/exception/exception.py
+++ network_security/exception/exception.py
@@ -6,9 +6,10 @@ class NetworkSecurityException(Exception):
         self.error_message = error_message
         _,_,exc_tb = error_details.exc_info()
 
-        self.lineno = exc_tb.tb_linenoself.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
 
 
     def __str__(self):
-        return "Error occured in pythin script name [{0}] line number [{1} error message [{2}]".format(
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
