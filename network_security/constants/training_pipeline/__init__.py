9cfad1efcfe47af4f55cbff175b4b014ad53212e
lezzycan <waheedolalekan23@gmail.com>
2026-09-16 08:23:31 +0100

    Add data ingestion module and related constants for data processing

---
+++ network_security/constants/training_pipeline/__init__.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+
+
+"""
+DATA Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = ""
