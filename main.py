9d96e289527aa350848deccc16bdff2733055685
lezzycan <waheedolalekan23@gmail.com>
2026-09-22 15:21:37 +0100

    Add logging and utility functions for data ingestion and validation
    - Created multiple log files for data ingestion processes with timestamps.
    - Implemented utility functions for reading and writing YAML files in the utils module.
    - Introduced error handling with custom exceptions for better debugging.
    - Added necessary imports and structured the utils module for future enhancements.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig
 import sys
 
 
@@ -12,7 +13,16 @@ if __name__ == "__main__":
         dataingestionconfig = DataIngestionConfig(training_pipeline_config=trainingPipelineConfig)
         dataingestion = DataIngestion(data_ingestion_config=dataingestionconfig)
         logging.info("initiate data ingestion")
+        
         dataingestionartifacts = dataingestion.initiate_data_ingestion()
         print(dataingestionartifacts)
+        datavalidationconfig = DataValidationConfig(training_pipeline_config=trainingPipelineConfig)
+        data_validation = DataValidation(data_ingestion_artifact= dataingestionartifacts, data_validation_config= datavalidationconfig)
+        logging.info("initiate data ingestion")
+        data_validation_artifacts = data_validation.initiate_data_validation()
+        print(data_validation_artifacts)
+        logging.info("data validation completed")
+        
+        
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- test_data.py
+++ test_data.py
@@ -1,11 +1,13 @@
 
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-
+import certifi
 uri = "mongodb+srv://Lezzycan:<@password>@cluster0.id99zq4.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+
+
+client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 
 # Send a ping to confirm a successful connection
 try:
